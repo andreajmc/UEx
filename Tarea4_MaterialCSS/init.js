e34e5a883b8878dcd19c62a554659a1fb25d32f0
andreajmc <40843579+andreajmc@users.noreply.github.com>
2020-02-08 19:00:51 -0600

    Create init.js

---
+++ Tarea4_MaterialCSS/init.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+
+    
+    // dropdown jQuery
+    $('.dropdown-trigger').dropdown();
+  
+  }); // end of document ready
+  
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+  
+})(jQuery); // end of jQuery name space
+
+
+
+
